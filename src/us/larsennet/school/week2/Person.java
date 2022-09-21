ad71710621b6846ac2f261b3483cea2ebc689b48
Jacob Larsen <jacobm@larsen.net>
2022-09-21 07:42:11 -0600

    Lab2a

--- src/us/larsennet/school/week2/Person.java
+++ src/us/larsennet/school/week2/Person.java
@@ -1,56 +1,71 @@
+//**
+//*File:lab2a
+//* Description:
+//Lessons Learned:
+//* Construct 1
+//* Construct 2
+//* Instructor's Name: Barbara Chamberlin
+//*
+//@author: William Spencer, Jacob Larsen
+//* since: 9/21/2022
+//*/
+
+
 package us.larsennet.school.week2;
 
 import java.time.LocalDate;
 
 public class Person {
-    private String name;
-    private LocalDate birthDate;
-    private String gender;
-    private String phone;
+	private String name;
+	private String gender;
+	private String phone;
+	private LocalDate birthDate;
 
-    public Person() {
-        this.name = "John Doe";
-        this.birthDate = LocalDate.parse("1900-01-01");
-        this.gender = "gender";
-        this.phone = "000-000-0000";
-    }
+	public Person() {
+		this.name = "John Doe";
+		this.gender = "undef";
+		this.phone = "000-000-0000";
+		this.birthDate = LocalDate.parse("1900-01-01");
+	}
 
-    public Person(String name, String dob, String gender, String phone) {
-        this.name = name;
-        this.birthDate = LocalDate.parse(dob);
-        this.gender = gender;
-        this.phone = phone;
-    }
+	public Person(String n, String g, String p, String dob) {
+	 this.name = n;
+	 this.gender = g;
+	 this.phone = p;
+	 // Since dob will always be in ISO format "yyyy-mm-dd" we can just use standard parse without format
+	 this.birthDate = LocalDate.parse(dob);
+ }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setBirthDate(String dob) {
-        this.birthDate = LocalDate.parse(dob);
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	// Since dob will always be in ISO format "yyyy-mm-dd" we can just use standard parse without format
+	public void setBirthDate(String dob) {
+		this.birthDate = LocalDate.parse(dob);
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
-}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+}//end of lab2a
