30614a9d7e194be07e428238580a6bdc8f19a9d0
Jacob Larsen <jacobm@larsen.net>
2022-09-21 09:30:54 -0600

    Lab2b create

---
+++ src/us/larsennet/school/week2/JUnitTesting.java
@@ -0,0 +1,16 @@
+//**
+//* File:lab2b
+//* Description:
+//  Lessons Learned:
+//*     Construct 1
+//*     Construct 2
+//* Instructor's Name: Barbara Chamberlin
+//*
+//@author: William Spencer, Jacob Larsen
+//* since: 9/21/2022
+//*/
+
+package us.larsennet.school.week2;
+
+public class JUnitTesting {
+}
