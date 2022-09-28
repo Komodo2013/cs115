b308fb659fd66a42905c5e0d049d35b84b25518b
Jacob Larsen <jacobm@larsen.net>
2022-09-28 12:09:27 -0600

    DessertShop

---
+++ src/us/larsennet/school/week3/IceCreamTest.java
@@ -0,0 +1,24 @@
+package us.larsennet.school.week3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IceCreamTest {
+
+    @Test
+    void setScoops() {
+    }
+
+    @Test
+    void setPricePerScoop() {
+    }
+
+    @Test
+    void getScoops() {
+    }
+
+    @Test
+    void getPricePerScoop() {
+    }
+}
---
+++ src/us/larsennet/school/week3/SundaeTest.java
@@ -0,0 +1,16 @@
+package us.larsennet.school.week3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SundaeTest {
+
+    @Test
+    void getTopping() {
+    }
+
+    @Test
+    void setTopping() {
+    }
+}
