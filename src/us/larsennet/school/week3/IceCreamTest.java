b4988be94d31721acd4e872a5c83c001af61cd4f
Jacob Larsen <jacobm@larsen.net>
2022-09-28 13:52:20 -0600

    DessertShop

--- src/us/larsennet/school/week3/IceCreamTest.java
+++ src/us/larsennet/school/week3/IceCreamTest.java
@@ -8,17 +8,33 @@ class IceCreamTest {
 
     @Test
     void setScoops() {
+        IceCream t = new IceCream();
+        t.setScoops(5);
+        assertEquals(5, t.getScoops());
+
+        t.setScoops(-5);
+        assertEquals(1, t.getScoops());
     }
 
     @Test
     void setPricePerScoop() {
+        IceCream t = new IceCream();
+        t.setPricePerScoop(4.99);
+        assertEquals(4.99, t.getPricePerScoop());
+
+        t.setPricePerScoop(-1.25);
+        assertEquals(0.0, t.getScoops());
     }
 
     @Test
     void getScoops() {
+        IceCream t = new IceCream("", 5, 1.1);
+        assertEquals(5, t.getScoops());
     }
 
     @Test
     void getPricePerScoop() {
+        IceCream t = new IceCream("", 5, 4.99);
+        assertEquals(4.99, t.getPricePerScoop());
     }
 }
